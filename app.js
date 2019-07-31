09a451a5b3500d78131a888419370d8b58bfd9ac
Matt Henley <mattbhenley@gmail.com>
2019-07-31 17:25:02 -0500

    changed to imgs, deleted intro

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 new hoverEffect({
     parent: document.querySelector(".distortion"),
     intensity: 0.2,
-    image1: "./bw1.png",
-    image2: "./bw2.jpg",
-    displacementImage: "./diss.png"
+    image1: "bw1.jpg",
+    image2: "bw2.jpg",
+    speedIn: 2,
+    speedOut: 2,
+    displacementImage: "diss.png"
 });
